58c7ad5e8ecade53eef1ba8937c1df8dfe250de6
sungwon.cha <mygenie3@naver.com>
2019-08-11 16:43:06 +0900

    fix

--- src/Demo/Demo.tsx
+++ src/Demo/Demo.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import GithubCorner from 'react-github-corner';
 import styles from './Demo.module.scss';
+import sampleImg from './yes_we_code.jpeg';
 
 function Demo(props: any) {
   return (
     <div className={styles.root}>
-      <h1>Demo</h1>
-      <GithubCorner
-        href="https://bluewings.github.io/react-genie-effect"
-        bannerColor="#007bff"
-      />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>react-genie-effect</h1>
+            <img src={sampleImg} width="224" />
+          </div>
+        </div>
+      </div>
+      <GithubCorner href="https://bluewings.github.io/react-genie-effect" />
     </div>
   );
 }
